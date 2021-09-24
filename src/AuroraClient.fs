d227a0dcbe29f30313fbb963a048eaee6852ba6e
Vladimir Shchur <odindafna2006@rambler.ru>
2021-09-24 14:58:46 +0300

    Project initialized

---
+++ src/AuroraClient.fs
@@ -0,0 +1,13 @@
+﻿namespace AuroraDataApiClient
+
+open Amazon.RDSDataService
+open Amazon.RDSDataService.Model
+
+type AuroraClient(client: AmazonRDSDataServiceClient) =
+    
+    //let createExecuteRequest sqlCommand parameters =     
+    member this.ExecuteSql(sqlCommand, parameters) =
+        task {
+            let! data = client.ExecuteStatementAsync <| ExecuteStatementRequest()
+            return data.NumberOfRecordsUpdated
+        }
